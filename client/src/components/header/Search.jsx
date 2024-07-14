271836b9d5e6d3c31a7acdc106e6c31bcbbf548a
Amiyansu Patra <139028543+Amiyansu@users.noreply.github.com>
2024-07-14 22:36:25 +0530

    header_Part

---
+++ client/src/components/header/Search.jsx
@@ -0,0 +1,34 @@
+import { Box, InputBase, styled } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+import React from "react";
+
+const SearchContainer = styled(Box)`
+  background: #fff;
+  width: 38%;
+  border-radius: 7px;
+  margin-left: 10px;
+  display: flex;
+`;
+const InputSearchBase = styled(InputBase)`
+  padding-left: 20px;
+  width: 100%;
+  font-size:unset;
+`;
+
+const SearchIconWrapper = styled(Box)`
+color:red;
+padding:5px;
+`;
+
+function Search() {
+  return (
+    <SearchContainer>
+      <InputSearchBase placeholder="Search for products, brand and more..." />
+      <SearchIconWrapper>
+        <SearchIcon />
+      </SearchIconWrapper>
+    </SearchContainer>
+  );
+}
+
+export default Search;
